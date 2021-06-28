e754dbde0b480936bc9500430601bb96002e1581
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-28 21:20:03 +0900

    Loginコンポーネントでimport, module.cssを定義。

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Login.module.css';
+
+import {
+  editPassword,
+  editUsername,
+  toggleMode,
+  fetchAsyncLogin,
+  fetchAsyncRegister,
+  selectAuthen,
+  selectIsLoginView,
+} from './loginSlice';
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const authen = useSelector(selectAuthen);
+  const isLoginView = useSelector(selectIsLoginView);
+  //usernameとpasswordどちらかが空文字であるかの判別式
+  const btnDisabler = authen.username === '' || authen.password === '';
+
   return <div>Hello login</div>;
 };
 
