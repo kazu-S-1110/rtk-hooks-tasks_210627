1f474cc8bec261059bb71f69298be51dc212bfe0
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-28 13:14:07 +0900

    非同期関数を追記。extraReducerを定義。

--- src/features/login/loginSlice.js
+++ src/features/login/loginSlice.js
@@ -1,7 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { act } from 'react-dom/test-utils';
 
 const apiUrl = 'http://localhost:8000/';
+const token = localStorage.localJWT;
 
 const initialState = {
   authen: {
@@ -24,6 +26,27 @@ export const fetchAsyncLogin = createAsyncThunk('login/post', async (auth) => {
   return res.data;
 });
 
+export const fetchAsyncRegister = createAsyncThunk(
+  'login/register',
+  async (auth) => {
+    const res = await axios.post(`${apiUrl}api/register`, auth, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncProf = createAsyncThunk('login/get', async () => {
+  const res = await axios.get(`${apiUrl}api/myself/`, {
+    headers: {
+      Authorization: `JWT ${token}`,
+    },
+  });
+  return res.data;
+});
+
 const loginSlice = createSlice({
   name: 'login',
   initialState,
@@ -38,4 +61,17 @@ const loginSlice = createSlice({
       state.isLoginView = !state.isLoginView;
     },
   },
+
+  //非同期関数の挙動（fulfilled,rejected,prevented）を定義できるのがextraReducers
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncLogin.fulfilled, (state, action) => {
+      localStorage.setItem('localJWT', action.payload.access);
+      action.payload.access && (window.location.href = '/tasks'); //accessキーがあればtasksに飛ぶよう論理積を定義。
+    });
+    builder.addCase(fetchAsyncProf.fulfilled, (state, action) => {
+      state.profile = action.payload;
+    });
+  },
 });
+
+export const { editUsername, editPassword, toggleMode } = loginSlice.actions;
