b20c7dba3136536fb7d696b2d98e3c5291be0e0a
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-28 21:32:53 +0900

    cssを追記。display部分を実装。

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -20,7 +20,41 @@ const Login = () => {
   //usernameとpasswordどちらかが空文字であるかの判別式
   const btnDisabler = authen.username === '' || authen.password === '';
 
-  return <div>Hello login</div>;
+  return (
+    <div className={styles.containerLogin}>
+      <div className={styles.appLogin}>
+        <h1>{isLoginView ? 'Login' : 'Register'}</h1>
+        <span>Username</span>
+        <input
+          type="text"
+          name="username"
+          placeholder="input your username"
+          className={styles.inputLog}
+          onChange={(e) => dispatch(editUsername(e.target.value))}
+          required
+        />
+        <span>Password</span>
+        <input
+          type="password"
+          className={styles.inputLog}
+          name="password"
+          placeholder="input your password"
+          onChange={(e) => dispatch(editPassword(e.target.value))}
+          required
+        />
+        <div className={styles.switch}>
+          <Button
+            variant="contained"
+            disabled={btnDisabler}
+            color="primary"
+            onClick={login}
+          >
+            {isLoginView ? 'Login' : 'Create'}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
