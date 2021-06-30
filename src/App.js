c8460c06bb90a53808a77571b850334246990cfd
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-30 12:37:12 +0900

    appコンポーネントを作成。

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './App.module.css';
-
+import { FaSignInAlt } from 'react-icons/fa';
 
 function App() {
+  const Logout = () => {
+    localStorage.removeItem('localJWT');
+    window.location.href = '/';
+  };
+
   return (
-    
-  )
+    <div className={styles.containerTasks}>
+      <div className={styles.appTasks}>
+        <button className={styles.signBtn} onClick={Logout}>
+          <FaSignInAlt />
+        </button>
+      </div>
+      <div className={styles.appDetails}></div>
+    </div>
+  );
 }
 
 export default App;
